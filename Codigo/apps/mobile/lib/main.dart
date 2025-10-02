8a32de175242d386ff4e91bc3db7d39671e18312
Laura Menezes <laura.heraclito@gmail.com>
2025-10-02 01:30:57 -0300

    tela-cadastro

--- Codigo/apps/mobile/lib/main.dart
+++ Codigo/apps/mobile/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 // import 'package:flutter_riverpod/flutter_riverpod.dart'; // Removido
 import 'features/checkin/presentation/screens/checkin.screen.dart';
 import 'features/user/presentation/screen/login.user.dart';
+import 'features/user/presentation/screen/cadastro.user.dart';
 
 void main() {
   // runZonedGuarded continua sendo útil para capturar erros
